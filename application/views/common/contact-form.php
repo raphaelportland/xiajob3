7fe6f6b75fa71beb1ab1bcd186a28264350d6942
R. Portland <raph_doppler@hotmail.com>
2012-11-27 15:19:19 +0100

    formulaire de contact v2

--- application/views/common/contact-form.php
+++ application/views/common/contact-form.php
@@ -1,6 +1,21 @@
-<div class='centered'>
-    
-<h2>Contactez nous</h2>
+<?php
+
+    if(isset($user->email)) {
+        $user_email = $user->email;
+    } else {
+        $user_email = '';
+    }
+        
+
+?>
+
+<center>
+    <img src="<?= base_url().'public/img/timbre.jpg'; ?>" />   
+    <h2>Laissez-nous un message !</h2>    
+</center>
+
+<p>Des idées, l’envie de participer, un mot gentil ou même une frustration à évacuer… n’hésitez pas.<br /> 
+Sans vos retours, on ne peut pas améliorer le site. Nous répondons, à tout le monde, au plus vite !</p>
 
 <?php echo validation_errors(); ?>
 
@@ -18,7 +33,7 @@ $subject = array(
 $email = array(
     'name'  => 'email',
     'id'    => 'email',
-    'value' => set_value('email'),
+    'value' => set_value('email', $user_email),
     'placeholder' => 'votre email pour la réponse',
     'maxlength' => 255,
     'size'  => 30,
@@ -30,7 +45,44 @@ $message = array(
     'placeholder' => 'votre message',
     'value' => set_value('message'),
 );
+
 ?>
+
+<h3>Pourquoi vous nous contactez ?</h3>
+
+<div class="control-group">
+    <label class="radio">
+        <input type='radio' name='motive' value='happy' <?php echo set_radio('motive', 'happy'); ?>/>
+        Je veux juste laisser un message sympa
+    </label>
+
+    <label class="radio">
+    <input type='radio' name='motive' value='participate' <?php echo set_radio('motive', 'participate'); ?>/>
+    Je suis très content(e) du site et j’ai quelque chose à dire
+    </label>
+
+    <label class="radio">
+    <input type='radio' name='motive' value='bug' <?php echo set_radio('motive', 'bug'); ?>/>
+    Je veux signaler un problème
+    </label>
+
+    <label class="radio">
+    <input type='radio' name='motive' value='team' <?php echo set_radio('motive', 'team'); ?>/>
+    Je suis pas content(e) et j’ai de bonnes raisons !
+    </label>
+
+    <label class="radio">
+    <input type='radio' name='motive' value='participate' <?php echo set_radio('motive', 'participate'); ?>/>
+    Je veux parler à l’équipe (et ça rentre pas dans les cases du dessus)
+    </label>
+</div>
+
+
+<p>Mille mercis à tous ceux qui ont écrit des messages sympa.<br />
+Nous avons chacun une activité à coté et vos encouragements ne sont pas de trop ;)</p>
+
+
+<h3>Votre message</h3>
 
 <div class="control-group">
 <?php echo form_label('Sujet', 'subject'); ?>
@@ -38,7 +90,7 @@ $message = array(
 </div>
 
 <div class="control-group">
-<?php echo form_label('Adresse Email', 'email'); ?>
+<?php echo form_label('Votre addresse email pour la réponse', 'email'); ?>
 <?php echo form_input($email); ?>
 </div>
 
@@ -52,5 +104,3 @@ $message = array(
 <?php echo form_submit('submit', 'Envoyer', 'class="btn btn-success"'); ?>
 </div>
 <?php echo form_close(); ?>
-
-</div>
