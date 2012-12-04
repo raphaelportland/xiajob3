b569b3d7b0b9367e4264e84bf37daba6c380f9ee
R. Portland <raph_doppler@hotmail.com>
2012-12-04 11:09:01 +0100

    nouvelle page Profil activé

--- application/views/candidat/registration/activated.php
+++ application/views/candidat/registration/activated.php
@@ -1,7 +1,9 @@
-<h2>Félicitations, votre compte est activé !</h2>
+<h2>Compte activé ! Flower Power ON :)</h2>
 
-<p class='lead'>Merci d'avoir choisi florBooks. Vous pouvez désormais créer vos books et poursuivre le remplissage de votre profil à l'étape suivante.</p>
+<p class='lead'>Si vous aimez l’art Floral, vous êtes au bon endroit !<br />
+Il vous reste 2 choses à faire pour rejoindre la communauté :<br />
+- remplir votre profil Pro<br />
+- créer votre premier florBooks<br /></p>
 
-<?php echo anchor('main',"Continuer",'class="btn btn-primary btn-large"'); ?>
-
+<?php echo anchor('main',"GO !",'class="btn btn-primary btn-large"'); ?>
 
