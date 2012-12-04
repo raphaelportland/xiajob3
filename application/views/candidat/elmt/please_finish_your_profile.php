dd9aa0ed4e6886f25e3af0bef9783f91ab308900
R. Portland <raph_doppler@hotmail.com>
2012-12-04 11:12:05 +0100

    "main" suite
    un oubli

--- application/views/candidat/elmt/please_finish_your_profile.php
+++ application/views/candidat/elmt/please_finish_your_profile.php
@@ -2,5 +2,5 @@
   <button type="button" class="close" data-dismiss="alert alert-info">×</button>
   <h4>Attention !</h4>
   Vous n'avez pas fini de remplir votre profil 
-  <?php echo anchor('fleurjob/edit_profile','cliquer ici pour terminer','class=""'); ?>  
+  <?php echo anchor('main/edit_profile','cliquer ici pour terminer','class=""'); ?>  
 </div>
