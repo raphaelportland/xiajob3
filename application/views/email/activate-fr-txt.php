a308b0f5d9d6a6c8cca757d2f75cfe6491d42e53
R. Portland <raph_doppler@hotmail.com>
2012-12-14 17:00:14 +0100

    email de demande d'activation mis à jour

--- application/views/email/activate-fr-txt.php
+++ application/views/email/activate-fr-txt.php
@@ -1,16 +1,12 @@
-Bienvenue sur <?php echo $site_name; ?>,
+Bienvenue chez florBooks... et merci !
 
-Nous vous remercions pour votre inscription à <?php echo $site_name; ?>. Nous avons listé les détails de votre compte ci-dessous, faites attention à les garder en sécurité.
+Nous avons listé les détails de votre compte ci-dessous, à garder précieusement !
 Pour confirmer votre adresse email, veuillez suivre ce lien :
 
 <?php echo site_url('/auth/activate/'.$user_id.'/'.$new_email_key.'/'.$password); ?>
 
 
 Vous devez confirmer votre adresse email dans les <?php echo $activation_period; ?> heures. Passé ce délai votre compte sera supprimé et vous devrez vous réinscrire.
-<?php if (strlen($username) > 0) { ?>
-
-Your username: <?php echo $username; ?>
-<?php } ?>
 
 Votre identifiant : <?php echo $email; ?>
 <?php if (isset($password)) {  ?>
@@ -18,5 +14,5 @@ Votre identifiant : <?php echo $email; ?>
 Votre mot de passe : <?php echo $password; ?>
 <?php  } ?>
 
-Bien cordialement,
-L'Equipe <?php echo $site_name; ?>
+Bien Floralement,
+L'Equipe florBooks
