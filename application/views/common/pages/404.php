1cf384e4a0d8b95116e0b72bc93189225406d553
R. Portland <raph_doppler@hotmail.com>
2013-01-10 16:44:24 +0100

    page 404 remaniée

--- application/views/common/pages/404.php
+++ application/views/common/pages/404.php
@@ -1,17 +1,24 @@
 
-<div class='big centered'>
+<div class='row'>
+    <div class='span6'>
+        <br />
+        <img src='<?= base_url().'public/img/ane.png'; ?>' />   
+    </div>
     
-    <img src='<?= base_url().'public/img/ane.png'; ?>' />
+    <div class='span6'>
+        <div class='big centered'>
+        <br /><br /><br />
+        <p>Il semble que notre informatique n’ait pas envie de faire ça. <br />
+        Nous sommes désolés, elle est parfois têtue.</p>
+        <p>Pouvez-vous nous décrire le problème ?</p>
+        
+        
 
-<p>Il semble que notre informatique n’ait pas envie de faire ça. <br />
-Nous sommes désolés, elle est parfois têtue.</p>
-<p>Pouvez-vous nous décrire le problème ?</p>
-
-
-<p class='medium'><i>Décrire le problème en 2 lignes et <?php echo anchor('social/contact','nous envoyer un message'); ?></i></p>
-
-<?php //$this->load->view('common/contact-form'); ?>
-
-<p class='huge'>:)</p>
-
+        
+        </div>        
+    </div>
+</div>
+<div class='big centered'>
+    <p class='medium'><i>Décrire le problème en 2 lignes et <?php echo anchor('social/contact','nous envoyer un message'); ?></i></p>
+    <p class='huge'>:)</p>
 </div>
