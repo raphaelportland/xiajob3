2d48bbf1da1d1773fb211d2d189bc53915880982
Pankaj <teamw@synapse.co.in>
2013-04-11 12:03:02 +0530

    Update wallhome.php

--- application/views/candidat/wallhome.php
+++ application/views/candidat/wallhome.php
@@ -148,7 +148,7 @@ function updateLikeDislike(formId) {
 	?>
         <div class="lower_cont">
         	<div class="portfolio_name" style="padding-top:15px;">
-            	<h4><?php echo $val->book_name;?> - by <?php echo $val->owner->first_name;?> <span>(<?php echo $val->pic_nb;?> photos)</span></h4>
+            	<h2><?php echo $val->book_name;?> - by <?php echo $val->owner->first_name;?> <span>(<?php echo $val->pic_nb;?> photos)</span></h2>
                 <p><?php echo $val->description;?>, <?php echo $val->owner->first_name;?></p>
             </div>
             
